78213a89e8eb3192242145921d0191584af5eb4c
adaoudi-ac <159470567+adaoudi-ac@users.noreply.github.com>
2024-03-16 16:06:34 +0100

    Update zenas_app_main.py

--- zenas_app_main.py
+++ zenas_app_main.py
@@ -7,10 +7,10 @@ import pandas
 st.title('Zena\'s Amazing Athleisure Catalog')
 
 # connect to snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-# cnx = st.connection("snowflake")
-# session = cnx.session()
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+cnx = st.connection("snowflake")
+my_cur = cnx.cursor()
 
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select color_or_style from catalog_for_website")
