944fede5ef2659262c08da640ceb9efd8b7b1e16
adaoudi-ac <159470567+adaoudi-ac@users.noreply.github.com>
2024-03-16 15:59:52 +0100

    Update zenas_app_main.py
    test

--- zenas_app_main.py
+++ zenas_app_main.py
@@ -1,5 +1,7 @@
-import streamlit
-import snowflake.connector
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
 import pandas
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
