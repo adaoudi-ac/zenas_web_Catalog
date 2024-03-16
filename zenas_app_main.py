325977fa4ab262ba38a4cbcb78f36517048d4a84
adaoudi-ac <159470567+adaoudi-ac@users.noreply.github.com>
2024-03-16 16:07:38 +0100

    Update zenas_app_main.py
    fix code

--- zenas_app_main.py
+++ zenas_app_main.py
@@ -30,5 +30,16 @@ option = st.selectbox('Pick a sweatsuit color or style:', list(color_list))
 
 # We'll build the image caption now, since we can
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
-#st.stop()
 
+# use the option selected to go back and get all the info from the database
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
+color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
+st.image(
+df2[0],
+width=400,
+caption= product_caption
+)
+st.write('Price: ', df2[1])
+st.write('Sizes Available: ',df2[2])
+st.write(df2[3])
