3a34b0ed10c11d26dd04087250f2f74e6d8a9b1c
adaoudi-ac <159470567+adaoudi-ac@users.noreply.github.com>
2024-03-16 16:05:18 +0100

    Update zenas_app_main.py
    test

--- zenas_app_main.py
+++ zenas_app_main.py
@@ -7,14 +7,14 @@ import pandas
 st.title('Zena\'s Amazing Athleisure Catalog')
 
 # connect to snowflake
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-cnx = st.connection("snowflake")
-session = cnx.session()
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+# cnx = st.connection("snowflake")
+# session = cnx.session()
 
 # run a snowflake query and put it all in a var called my_catalog
-session.execute("select color_or_style from catalog_for_website")
-my_catalog = session.fetchall()
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
 
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
