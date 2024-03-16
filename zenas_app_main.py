68d1b4fcdd04c9ebb95d7e3d24e79a79394190e5
adaoudi-ac <159470567+adaoudi-ac@users.noreply.github.com>
2024-03-16 16:03:47 +0100

    Update zenas_app_main.py
    test

--- zenas_app_main.py
+++ zenas_app_main.py
@@ -4,17 +4,17 @@ from snowflake.snowpark.functions import col
 import requests
 import pandas
 
-streamlit.title('Zena\'s Amazing Athleisure Catalog')
+st.title('Zena\'s Amazing Athleisure Catalog')
 
 # connect to snowflake
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
 cnx = st.connection("snowflake")
 session = cnx.session()
 
 # run a snowflake query and put it all in a var called my_catalog
 session.execute("select color_or_style from catalog_for_website")
-my_catalog = my_cur.fetchall()
+my_catalog = session.fetchall()
 
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
@@ -26,7 +26,7 @@ color_list = df[0].values.tolist()
 
 # print(color_list)
 # Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+option = st.selectbox('Pick a sweatsuit color or style:', list(color_list))
 
 # We'll build the image caption now, since we can
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
