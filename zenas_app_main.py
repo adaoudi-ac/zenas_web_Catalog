e92063e18a7aefd72f66e669e1f567b4474a1005
adaoudi-ac <159470567+adaoudi-ac@users.noreply.github.com>
2024-03-16 15:47:57 +0100

    Update zenas_app_main.py
    # Import python packages and connection

--- zenas_app_main.py
+++ zenas_app_main.py
@@ -1,3 +1,10 @@
+# Import python packages
 import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
+import pandas
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 st.title('My Parents New Helathy Dinner')
